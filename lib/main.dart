a3287a35c3773a614992c932a6d7d7d759f2b6cd
naeimDz <naeimdz1703@gmail.com>
2024-10-24 10:50:34 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screen/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Easy Localization
+  await EasyLocalization.ensureInitialized();
+
+  // Initialize Shared Preferences
+  final prefs = await SharedPreferences.getInstance();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [
+        Locale('en'),
+        Locale('ar')
+      ], // Add more locales as needed
+      path: 'assets/translations', // Folder for localization files
+      fallbackLocale: const Locale('en'),
+      child: const MyApp(),
+      /*child: MultiProvider(
+        providers: const [
+          // Add your global providers here
+        ],
+        child: const MyApp(),
+      ),*/
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,12 +38,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Colors.deepPurple),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      localizationsDelegates:
+          context.localizationDelegates, // Localization delegates
+      supportedLocales: context.supportedLocales, // Supported locales
+      locale: context.locale, // Current locale
       home: const HomeScreen(title: 'Flutter Demo Home Page'),
     );
   }
